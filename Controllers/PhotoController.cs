1c28883122de9d89e60c156bffbec3a2f98809a9
gustavo <gustavorodri2011@gmail.com>
2023-02-13 00:04:09 -0300

    cambios a la api para recibir la imagen

---
+++ Controllers/PhotoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_images.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post()
+        {
+            var file = Request.Form.Files[0];
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "photos", file.FileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return Ok();
+        }
+    }
+}
